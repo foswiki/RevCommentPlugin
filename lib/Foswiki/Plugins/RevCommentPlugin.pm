feb72e4df4480678cd12a5dd016b5c4b0699cce4
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-01-22 17:59:09 +0000

    Item838: RevCommentPlugin does not have example images attached We are in 2009.
    git-svn-id: http://svn.foswiki.org/trunk/RevCommentPlugin@2125 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/RevCommentPlugin.pm
+++ lib/Foswiki/Plugins/RevCommentPlugin.pm
@@ -39,7 +39,7 @@ $VERSION = '$Rev: 9841 $';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-$RELEASE = '08 Dec 2008';
+$RELEASE = '22 Jan 2009';
 
 $pluginName = 'RevCommentPlugin';
 
